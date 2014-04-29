8679867571786810b9e0737bc4d066ae59b3f70e
wuyazi <xxiandong@gmail.com>
2014-04-29 21:15:35 +0800

    warm2, not finish

---
+++ greet/test/greet_spec.js
@@ -0,0 +1,10 @@
+var greet = require("../index.js");
+var input_string = "haha --drunk";
+
+describe('greet', function(){
+  it("should greet aperson by name", function(){
+    // console.dir(input_string);
+    // console.log(greet(input_string.slice(1)));
+    // expect(greet(input_string)[1]).to.be.null;
+  });
+});
